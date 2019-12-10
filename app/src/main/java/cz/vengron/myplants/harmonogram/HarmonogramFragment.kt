1f6c5f12eec92a194cad3e99a0029412b1cb6a3f
Venceslav-Chumchal <venceker@gmail.com>
2019-12-10 15:27:36 +0100

    Add Harmonogram fragment

---
+++ app/src/main/java/cz/vengron/myplants/harmonogram/HarmonogramFragment.kt
@@ -0,0 +1,24 @@
+package cz.vengron.myplants.harmonogram
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import cz.vengron.myplants.R
+import cz.vengron.myplants.databinding.HarmonogramFragmentBinding
+
+class HarmonogramFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: HarmonogramFragmentBinding = DataBindingUtil.inflate(
+            inflater, R.layout.harmonogram_fragment, container, false)
+
+        return binding.root
+    }
+}
