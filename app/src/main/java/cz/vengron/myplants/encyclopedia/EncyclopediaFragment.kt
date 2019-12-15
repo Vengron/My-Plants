58c7a6b99c01850bb9601767dc0cd4ca35f446e2
Věnceslav_Chumchal <venceker@gmail.com>
2019-12-15 09:38:42 +0100

    Update RecyclerView

--- app/src/main/java/cz/vengron/myplants/encyclopedia/EncyclopediaFragment.kt
+++ app/src/main/java/cz/vengron/myplants/encyclopedia/EncyclopediaFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import cz.vengron.myplants.R
 import cz.vengron.myplants.databinding.EncyclopediaFragmentBinding
 
@@ -18,6 +19,8 @@ class EncyclopediaFragment : Fragment( ){
     ): View? {
         val binding: EncyclopediaFragmentBinding = DataBindingUtil.inflate(
             inflater, R.layout.encyclopedia_fragment, container, false)
+        val manager = GridLayoutManager(activity, 3)
+        binding.plantsList.layoutManager = manager
         return binding.root
     }
 }
