76f124a645563b08fa3148d1170c65f52b8c669b
Věnceslav_Chumchal <venceker@gmail.com>
2020-01-11 17:21:05 +0100

    Move URLs of images of plants to Fragment from ViewModel

--- app/src/main/java/cz/vengron/myplants/plantaddition/PlantAdditionFragment.kt
+++ app/src/main/java/cz/vengron/myplants/plantaddition/PlantAdditionFragment.kt
@@ -20,6 +20,28 @@ class PlantAdditionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        val urlsOfImages = mapOf<String, String>(
+            Pair(
+                "Plectranthus amboinicus",
+                "https://upload.wikimedia.org/wikipedia/commons/1/1b/Leaf_-pani_koorkka.JPG"
+            ),
+            Pair(
+                "Plectranthus scutellarioides",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Starr_070830-8251_Solenostemon_scutellarioides.jpg/675px-Starr_070830-8251_Solenostemon_scutellarioides.jpg"
+            ),
+            Pair(
+                "Aloe vera",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Alo%C3%AB-vera-habitus.jpg/600px-Alo%C3%AB-vera-habitus.jpg"
+            ),
+            Pair(
+                "Piper nigrum",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Piper_longum_plant.jpg/800px-Piper_longum_plant.jpg"
+            ),
+            Pair(
+                "Spathiphyllum cochlearispathum",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Spathiphyllum_cochlearispathum_RTBG.jpg/800px-Spathiphyllum_cochlearispathum_RTBG.jpg"
+            )
+        )
 
         val binding: PlantAdditionFragmentBinding = DataBindingUtil.inflate(
             inflater, R.layout.plant_addition_fragment, container, false
--- app/src/main/java/cz/vengron/myplants/plantaddition/PlantAdditionViewModel.kt
+++ app/src/main/java/cz/vengron/myplants/plantaddition/PlantAdditionViewModel.kt
@@ -7,29 +7,6 @@ import kotlinx.coroutines.*
 
 class PlantAdditionViewModel(val database: PlantsDatabaseDao) : ViewModel() {
 
-    enum class NameOfPlants(latinName: String, imageUrl: String) {
-        RYMOVNIK(
-            "Plectranthus amboinicus",
-            "https://upload.wikimedia.org/wikipedia/commons/1/1b/Leaf_-pani_koorkka.JPG"
-        ),
-        KOPRIVA(
-            "Plectranthus scutellarioides",
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Starr_070830-8251_Solenostemon_scutellarioides.jpg/675px-Starr_070830-8251_Solenostemon_scutellarioides.jpg"
-        ),
-        ALOE(
-            "Aloe vera",
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Alo%C3%AB-vera-habitus.jpg/600px-Alo%C3%AB-vera-habitus.jpg"
-        ),
-        PEPROVNIK(
-            "Piper",
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Piper_longum_plant.jpg/800px-Piper_longum_plant.jpg"
-        ),
-        LOPATKOVEC(
-            "Spathiphyllum",
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Spathiphyllum_cochlearispathum_RTBG.jpg/800px-Spathiphyllum_cochlearispathum_RTBG.jpg"
-        )
-    }
-
     private var viewModelJob = Job()
 
     override fun onCleared() {
