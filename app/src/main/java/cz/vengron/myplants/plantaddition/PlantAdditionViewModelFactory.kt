3141471ec4473eb714e437dfcb17928a6081fddb
Věnceslav_Chumchal <venceker@gmail.com>
2020-01-11 16:37:23 +0100

    Add factory for PlantAdditionViewModel

---
+++ app/src/main/java/cz/vengron/myplants/plantaddition/PlantAdditionViewModelFactory.kt
@@ -0,0 +1,17 @@
+package cz.vengron.myplants.plantaddition
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import cz.vengron.myplants.database.PlantsDatabaseDao
+
+class PlantAdditionViewModelFactory(
+    private val dataSource: PlantsDatabaseDao
+) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(PlantAdditionViewModel::class.java)) {
+            return PlantAdditionViewModel(dataSource) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
