a12f4875a3eb07df781ebf1235ccd932bb31ee5c
Věnceslav_Chumchal <venceker@gmail.com>
2020-01-18 18:06:18 +0100

    Add new DAO functions

---
+++ app/src/androidTest/java/cz/vengron/myplants/PlantsDatabaseTest.kt
@@ -0,0 +1,53 @@
+package cz.vengron.myplants
+
+import androidx.room.Room
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import cz.vengron.myplants.database.Plant
+import cz.vengron.myplants.database.PlantsDatabase
+import cz.vengron.myplants.database.PlantsDatabaseDao
+import org.junit.After
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Before
+import java.io.IOException
+import java.lang.Exception
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class PlantDatabaseTest {
+
+    private lateinit var plantDao: PlantsDatabaseDao
+    private lateinit var db: PlantsDatabase
+
+    @Before
+    fun createDB() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        db = Room.inMemoryDatabaseBuilder(appContext, PlantsDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+        plantDao = db.plantsDatabaseDao
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertAndGetPlant() {
+        val plant = Plant()
+        plantDao.insert(plant)
+        val oldPlant = plantDao.getLastPlant()
+        assertEquals(oldPlant, null)
+    }
+}
--- app/src/main/java/cz/vengron/myplants/database/PlantsDatabaseDao.kt
+++ app/src/main/java/cz/vengron/myplants/database/PlantsDatabaseDao.kt
@@ -29,4 +29,10 @@ interface PlantsDatabaseDao {
      */
     @Query("SELECT * FROM plants_table ORDER BY plantId DESC")
     fun getAllPlants(): LiveData<List<Plant>>
+
+    @Query("SELECT * FROM plants_table WHERE plantId = :key")
+    fun getPlantWithId(key: Long): LiveData<Plant>
+
+    @Query("SELECT * FROM plants_table ORDER BY plantId DESC limit 1")
+    fun getLastPlant(): Plant
 }
