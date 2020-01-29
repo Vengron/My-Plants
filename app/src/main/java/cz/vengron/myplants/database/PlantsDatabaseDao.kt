83ea0d93ea6c8e8a7b268b180ed9c94c35520d4c
Věnceslav_Chumchal <venceker@gmail.com>
2020-01-29 20:15:05 +0100

    Remove unnecessary method from DAO

--- app/src/main/java/cz/vengron/myplants/database/PlantsDatabaseDao.kt
+++ app/src/main/java/cz/vengron/myplants/database/PlantsDatabaseDao.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.room.*
 
 /**
- * Defines methods for using the[Plant] class with Room
+ * Defines methods for using the [Plant] class with Room
  */
 
 @Dao
@@ -16,17 +16,6 @@ interface PlantsDatabaseDao {
     @Update
     fun update(plant: Plant)
 
-    /**
-     * Method used in case of clicking on item in RecyclerView
-     * on CollectionFragment
-     * @param [key] is [Plant.plantId]
-     */
-    @Query("SELECT * FROM plants_table WHERE plantId = :key")
-    fun get(key: Long): Plant?
-
-    /**
-     * Method used for getting all plants for RecyclerView
-     */
     @Query("SELECT * FROM plants_table ORDER BY plantId DESC")
     fun getAllPlants(): LiveData<List<Plant>>
 
