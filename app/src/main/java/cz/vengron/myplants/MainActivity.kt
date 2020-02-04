5f9cb81cf5c02106bdeeaae842ed86c294223aca
Věnceslav_Chumchal <venceker@gmail.com>
2020-02-04 13:16:15 +0100

    Implement navigation between bottom menu fragments

--- app/src/main/java/cz/vengron/myplants/MainActivity.kt
+++ app/src/main/java/cz/vengron/myplants/MainActivity.kt
@@ -2,11 +2,26 @@ package cz.vengron.myplants
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.collectionFragment, R.id.scheduleFragment, R.id.settingsFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
